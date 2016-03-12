0441d2eaf09a226063c84b9af6dd64801b1a7dc7
Alco <alcohol49@gmail.com>
2016-03-12 23:07:25 +0800

    Sign up for Google API & get now position

--- app/src/main/java/com/hackathon/tourguard/MapsActivity.java
+++ app/src/main/java/com/hackathon/tourguard/MapsActivity.java
@@ -1,5 +1,7 @@
 package com.hackathon.tourguard;
 
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -38,9 +40,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        LatLng sydney = new LatLng(location.getLatitude(), location.getLongitude());
+
+
+        mMap.addMarker(new MarkerOptions().position(sydney).title("Start location"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
