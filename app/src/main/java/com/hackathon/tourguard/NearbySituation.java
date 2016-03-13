19395053a4fd1512e3e6e808adb4f6803db6988b
Alco <alcohol49@gmail.com>
2016-03-13 08:16:55 +0800

    Nearby situaction

--- app/src/main/java/com/hackathon/tourguard/NearbySituation.java
+++ app/src/main/java/com/hackathon/tourguard/NearbySituation.java
@@ -1,13 +1,34 @@
 package com.hackathon.tourguard;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ListActivity;
+import android.app.LoaderManager;
+import android.content.Loader;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class NearbySituation extends AppCompatActivity {
+public class NearbySituation extends ListActivity {
+
+    public String[] mStrings = {
+            "附近有龍捲風",
+            "附近有淹水情形",
+            "預定搭程班機因龍捲風取消"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nearby_situation);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, mStrings);
+        ListView listView = (ListView) findViewById(android.R.id.list);
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
     }
 }
